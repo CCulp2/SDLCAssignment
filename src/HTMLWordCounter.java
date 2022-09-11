f4051d042d07d11528f077952f687b4349b8232d
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-09-11 01:23:36 -0400

    Project Init with default HTML file.

---
+++ src/HTMLWordCounter.java
@@ -0,0 +1,9 @@
+import java.io.File;
+
+public class HTMLWordCounter {
+    public static void main(String[] args) {
+        File defaultFileToRead = new File("src/1065-h.htm");
+        File inputFile;
+
+    }
+}
