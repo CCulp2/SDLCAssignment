e8cb3f038272b03f39125386c86e7d8cc89c2227
CCulp2 <80595975+CCulp2@users.noreply.github.com>
2022-10-17 15:11:30 -0400

    Added JUnit Testing

---
+++ Test/StringSanitizerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringSanitizerTest {
+
+    @Test
+    void removeHTML() {
+    }
+
+    @Test
+    void removePunctuation() {
+    }
+
+    @Test
+    void sanitizeAndArrayList() {
+    }
+}
